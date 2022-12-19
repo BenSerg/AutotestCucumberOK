85d8b5be394dfc7a1721acb248ab8e7f98af258b
BenSerg <bendrysevsergej@gmail.com>
2022-12-19 22:46:15 +0300

    imports

--- src/main/java/Pages/DriverWaiter.java
+++ src/main/java/Pages/DriverWaiter.java
@@ -3,15 +3,13 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class DriverWaiter
 {
-  private WebDriver driver;
+  private final WebDriver driver;
 
   public DriverWaiter(WebDriver driver)
   {
